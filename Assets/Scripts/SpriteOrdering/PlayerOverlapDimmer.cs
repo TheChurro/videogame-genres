9f4222cf0ec5d59b64b34ec561950502726bfe43
John Churay <thechurroextrodinare@gmail.com>
2020-06-02 14:10:04 -0500

    Added house sprites and a script to hide house walls when inside a house.

---
+++ Assets/Scripts/SpriteOrdering/PlayerOverlapDimmer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOverlapDimmer : MonoBehaviour
+{
+    public SpriteRenderer[] to_dim;
+    public float dim_amount;
+    
+    public void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag != "Player") return;
+        if (col.isTrigger) return;
+        foreach (var dim in to_dim) {
+            dim.color = new Color(dim.color.r, dim.color.g, dim.color.b, dim_amount);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D col) {
+        if (col.tag != "Player") return;
+        if (col.isTrigger) return;
+        foreach (var dim in to_dim) {
+            dim.color = new Color(dim.color.r, dim.color.g, dim.color.b, 1);
+        }
+    }
+}
